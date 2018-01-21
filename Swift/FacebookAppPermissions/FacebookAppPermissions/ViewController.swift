9d7c2a7d8e2dd614367282e25abc167c270daf98
Aaron Miller <aaronmiller822028@gmail.com>
2018-01-20 20:53:29 -0800

    possibly will break things

--- Swift/FacebookAppPermissions/FacebookAppPermissions/ViewController.swift
+++ Swift/FacebookAppPermissions/FacebookAppPermissions/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController, FBSDKLoginButtonDelegate {
-
+class ViewController: UIViewController,
+FBSDKLoginButtonDelegate {
+    
+    
+    @IBOutlet weak var facebookBtn: UIButton!
+    
     let loginButton: FBSDKLoginButton = {
         let button = FBSDKLoginButton()
         button.readPermissions = ["email"]
@@ -19,11 +23,9 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.addSubview(loginButton)
         loginButton.center = view.center
         loginButton.delegate = self
-        
         if let token = FBSDKAccessToken.current(){
             fetchProfile()
         }
@@ -31,7 +33,6 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     func fetchProfile(){
         print("fetch profile-- ViewController.swift")
-        
         let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
         FBSDKGraphRequest(graphPath: "me", parameters: parameters).start{
             (connection, result, error) -> Void in
@@ -40,13 +41,31 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
                 return
             }
             guard let unwrappedResult = result as? [String: Any] else {return}
-            let email = unwrappedResult["email"]  as! String
-            print(email)
+            print(unwrappedResult)
+            if let email = unwrappedResult["email"]  as? String{
+                print(email)
+            }
+            guard let picture = unwrappedResult["picture"] as? NSDictionary, let data = picture["data"] as? NSDictionary else {return}
+            if let url = data["url"] as? String{
+                //link to my picture
+                print(url)
+            }
         }
     }
     
+    @IBAction func facebookBtnPressed(_ sender: Any) {
+//        let fbLoginManager = FBSDKLoginManager()
+//        fbLoginManager.logInWithReadPermissions(["email"], fromViewController )
+        
+    }
+    
+    
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error){
         print("Complete Login--ViewController.swift")
+//        if FBSDKAccessToken.current() != nil {
+//            fetchProfile()
+//            post()
+//        }
     }
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
@@ -62,6 +81,33 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func post(){
+        let parameters: [String: String] = ["access_token" : FBSDKAccessToken.current().tokenString ]
+        //https://prod1.mytcheck.com/auth/facebook
+        //http://httpbin.org/post
+        guard let url = URL(string: "https://prod1.mytcheck.com/auth/facebook") else {return}
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let myHttpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
+        request.httpBody = myHttpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                }catch {
+                    print(error)
+                }
+            }
+            }.resume()
+    }
+    
 }
 
